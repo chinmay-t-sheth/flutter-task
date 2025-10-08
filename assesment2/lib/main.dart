73f6cbeedac83e3c7ee8b05164cad9644db1541e
chinmay sheth <117623623+chinmay-t-sheth@users.noreply.github.com>
2025-10-08 19:16:16 +0530

    Create main.dart

---
+++ assesment2/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyNotes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const SplashScreen(),
+    );
+  }
+}
