d461f8cb2e3464b7abbe3b3f31f7a43aacd03a49
chinmay sheth <117623623+chinmay-t-sheth@users.noreply.github.com>
2025-10-14 10:26:09 +0530

    Create index.php

---
+++ career_guideline/index.php
@@ -0,0 +1,20 @@
+<?php
+echo "<h2>chinmayAPI</h2>";
+echo "<ul>";
+echo "<li><a href='chinmay/connect.php'>connection</a></li>";
+echo "<li><a href='chinmay/add_category.php'>add_category</a></li>";
+echo "<li><a href='chinmay/add_career.php'>add_career</a></li>";
+echo "<li><a href='chinmay/add_college.php'>add_college</a></li>";
+echo "<li><a href='chinmay/register.php'>register</a></li>";
+echo "<li><a href='chinmay/login.php'>login</a></li>";
+echo "<li><a href='chinmay/view_careers.php'>view_careers</a></li>";
+echo "<li><a href='chinmay/view_colleges.php'>view_colleges</a></li>";
+echo "<li><a href='chinmay/post_question.php'>post_question</a></li>";
+echo "<li><a href='chinmay/post_answer.php'>post_answer</a></li>";
+echo "<li><a href='chinmay/view_questions.php'>view_questions</a></li>";
+echo "<li><a href='chinmay/add_question.php'>add_test_question</a></li>";
+echo "<li><a href='chinmay/take_test.php'>take_test</a></li>";
+echo "<li><a href='chinmay/test_result.php'>test_result</a></li>";
+echo "<li><a href='chinmay/add_contribution.php'>add_contribution</a></li>";
+echo "</ul>";
+?>
